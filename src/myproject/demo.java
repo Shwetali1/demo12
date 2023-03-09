ca08ee857619d910e8e1c3169578ed26d74ad525
Shwetali1 <88235252+Shwetali1@users.noreply.github.com>
2023-03-09 16:19:00 +0530

    new changes

--- src/myproject/demo.java
+++ src/myproject/demo.java
@@ -1,10 +1,10 @@
 package myproject;
 
-public class demo {
+public class demo12 {
 
 	public static void main(String[] args) {
-		int n1 = 225, n2 = 115, sum;  
-		sum = n1 + n2;  
+		int n3 = 225, n2 = 115, sum;  
+		sum = n3 + n2;  
 		System.out.println("The sum of numbers is: "+sum);  
 
 	}
