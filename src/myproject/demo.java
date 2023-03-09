addc8d9ed4c05c2e45a6475b19354ffc7e8ad23f
Shwetali1 <tarwadeshwetali@gmail.com>
2023-03-09 11:01:04 +0530

    new change

--- src/myproject/demo.java
+++ src/myproject/demo.java
@@ -3,7 +3,9 @@ package myproject;
 public class demo {
 
 	public static void main(String[] args) {
-		
+		int n1 = 225, n2 = 115, sum;  
+		sum = n1 + n2;  
+		System.out.println("The sum of numbers is: "+sum);  
 
 	}
 
